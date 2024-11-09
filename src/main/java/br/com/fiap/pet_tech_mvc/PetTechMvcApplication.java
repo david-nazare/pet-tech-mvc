1ab197696aa9394a29e7589e079f57db3a3b961a
David Nazare <dsnazare@icloud.com>
2024-11-09 16:10:43 -0300

    Versao inicial

---
+++ src/main/java/br/com/fiap/pet_tech_mvc/PetTechMvcApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pet_tech_mvc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetTechMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetTechMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/pet_tech_mvc/PetTechMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.pet_tech_mvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetTechMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
